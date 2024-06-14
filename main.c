6d74288d0299de572142e8d1c218a9568450c564
gkatsarova <g_katsarova2007@abv.bg>
2024-06-14 16:28:45 +0300

    mai weche raboti

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define PLAYERS_ARRAY_SIZE 13
+#define NUM_PLAYERS 4
 
 struct Card {
     char value;
     char color;
 };
 
-int find_winner_by_4_cards(struct Card players[][PLAYERS_ARRAY_SIZE]) {
-    int count[4][PLAYERS_ARRAY_SIZE] = {0};
+int find_winner_by_4_cards(struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE]) {
+    int count[NUM_PLAYERS][PLAYERS_ARRAY_SIZE] = {0};
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < NUM_PLAYERS; i++) {
         for (int j = 0; j < PLAYERS_ARRAY_SIZE; j++) {
             int value = players[i][j].value;
             count[i][value]++;
@@ -21,9 +22,9 @@ int find_winner_by_4_cards(struct Card players[][PLAYERS_ARRAY_SIZE]) {
     return -1;
 }
 
-int find_winner_by_value(struct Card players[][PLAYERS_ARRAY_SIZE]){
-    int result[4] = {0};
-    for (int i = 0; i < 4; i++) {
+int find_winner_by_value(struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE]){
+    int result[NUM_PLAYERS] = {0};
+    for (int i = 0; i < NUM_PLAYERS; i++) {
         for (int j = 0; j < PLAYERS_ARRAY_SIZE; j++) {
             result[i] = result[i] + players[i][j].value;
         }
@@ -31,7 +32,7 @@ int find_winner_by_value(struct Card players[][PLAYERS_ARRAY_SIZE]){
 
     int max = result[0];
     int winner;
-    for (int i = 1; i < 4; i++) {
+    for (int i = 0; i < NUM_PLAYERS; i++) {
         if (result[i] > max) {
             max = result[i];
             winner = i + 1;
@@ -40,45 +41,69 @@ int find_winner_by_value(struct Card players[][PLAYERS_ARRAY_SIZE]){
     return winner;
 }
 
-int game(char* fileName) {
+int game(char* fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE]) {
     FILE* file = fopen(fileName, "r");
     if (file == NULL) {
         printf("Error opening file");
-        return -1;
+        exit(-1);
     }
-
-    struct Card players[4][PLAYERS_ARRAY_SIZE];
 
     char value;
     char color;
-    for (int i = 0; fscanf(file, " %c %c", &value, &color) == 2; i = (i + 1) % 4) {
+    int cards = 0;
+    while (fscanf(file, " %c %c", &value, &color) == 2) {
+        int player_index = cards % NUM_PLAYERS;
+        int card_index = cards / NUM_PLAYERS;
+
         if (value == 'J') value = 10;
         else if (value == 'Q') value = 12;
         else if (value == 'K') value = 15;
         else if (value == 'A') value = 20;
         else value -= '0';
 
-        players[i][i].value = value;
-        players[i][i].color = color;
+        players[player_index][card_index].value = value;
+        players[player_index][card_index].color = color;
 
-        printf("player[%d] value %d color %c\n", i + 1, value, color);
+        //printf("player[%d] value %d color %c\n", player_index + 1, value, color);
+        cards++;
     }
 
     fclose(file);
 
     int winner = find_winner_by_4_cards(players);
-    if(winner = -1) winner = find_winner_by_value(players);
+    if(winner == -1) winner = find_winner_by_value(players);
     return winner;
 }
 
+int check_value(int value){
+    if(value >= 1 && value <= 9) return (value + '0');
+    else if(value == 10) return 'J';
+    else if(value == 12) return 'Q';
+    else if(value == 15) return 'K';
+    else if(value == 20) return 'A';
+
+}
+
+void print_winners_array(int winner, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE]) {
+    int flag = 0;
+    for(int i = 1; i < winner + 1; i++){
+        if (winner == i) {
+            flag = 1;
+            printf("Player %d wins: ", winner);
+            for (int i = 0; i < PLAYERS_ARRAY_SIZE; i++) {
+                players[winner][i].value = check_value(players[winner][i].value);
+                printf("(%c) (%c); ", players[winner][i].value, players[winner][i].color);
+            }
+            printf("\n");
+        } 
+        if(flag == 1) break;
+    }
+}
+
 int main() {
-    int winner = game("teste.txt");
-
-    if (winner == 1) printf("The winner is player1!\n");
-    else if (winner == 2) printf("The winner is player2!\n");
-    else if (winner == 3) printf("The winner is player3!\n");
-    else printf("The winner is player4!\n");
+    struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE] = {0};
+    int winner = game("teste.txt", players);
     
-
+    print_winners_array(winner, players);
     return 0;
 }
