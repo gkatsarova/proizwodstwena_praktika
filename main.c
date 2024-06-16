bf5c87dc52c3f8c0ddd54887e5a497d8f16de9d1
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2024-06-17 00:22:08 +0300

    Криптиране

--- main.c
+++ main.c
@@ -1,42 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 #define PLAYERS_ARRAY_SIZE 13
 #define NUM_PLAYERS 4
 
-struct Card {
+struct Card
+{
     char value;
     char color;
 };
 
-int card_value(char value) {
-    if (value >= '1' && value <= '9') return value - '0';
-    if (value == 'J') return 10;
-    if (value == 'Q') return 12;
-    if (value == 'K') return 15;
-    if (value == 'A') return 20;
+int card_value(char value)
+{
+    if (value >= '1' && value <= '9')
+        return value - '0';
+    if (value == 'J')
+        return 10;
+    if (value == 'Q')
+        return 12;
+    if (value == 'K')
+        return 15;
+    if (value == 'A')
+        return 20;
 }
 
-int color_value(char color) {
-    if(color == 'C') return 1;
-    else if(color == 'D') return 2;
-    else if(color == 'H') return 3;
-    else return 4;
- }
+int color_value(char color)
+{
+    if (color == 'C')
+        return 1;
+    else if (color == 'D')
+        return 2;
+    else if (color == 'H')
+        return 3;
+    else if (color == 'S')
+        return 4;
+}
 
-int check_winner_by_4cards(int count[NUM_PLAYERS][13], struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE]) {
+int check_winner_by_4cards(int count[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE])
+{
     int winner = -1;
 
-    for (int i = 0; i < NUM_PLAYERS; i++) {
-        for (int j = 0; j < 13; j++) {
-            if (count[i][j] == 4) {
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        for (int j = 0; j < PLAYERS_ARRAY_SIZE; j++)
+        {
+            if (count[i][j] == 4)
+            {
                 int card_value_winner = j + 1;
 
-                if (winner != -1) {
+                if (winner != -1)
+                {
                     int current_value = card_value(players[winner][0].value);
-                    if (card_value_winner > current_value) {
+                    if (card_value_winner > current_value)
+                    {
                         winner = i;
                     }
-                } else {
+                }
+                else
+                {
                     winner = i;
                 }
 
@@ -47,21 +70,24 @@ int check_winner_by_4cards(int count[NUM_PLAYERS][13], struct Card players[NUM_P
 
     return winner;
 }
-int game(char* fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], int player_card_count[NUM_PLAYERS]) {
-    FILE* file = fopen(fileName, "r");
-    if (file == NULL) {
+int game(char *fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], int player_card_count[NUM_PLAYERS])
+{
+    FILE *file = fopen(fileName, "r");
+    if (file == NULL)
+    {
         printf("Error opening file\n");
         exit(1);
     }
 
-    int count[NUM_PLAYERS][13] = {0};
+    int count[NUM_PLAYERS][PLAYERS_ARRAY_SIZE] = {0};
     int winner_by_4_cards = -1;
     char value, color;
     int cards = 0;
-    while(fscanf(file, "%c %c ", &value, &color) == 2){
+    while (fscanf(file, "%c %c ", &value, &color) == 2)
+    {
         int player_index = cards % NUM_PLAYERS;
         int card_index = player_card_count[player_index];
-        
+
         players[player_index][card_index].value = value;
         players[player_index][card_index].color = color;
         player_card_count[player_index]++;
@@ -70,9 +96,11 @@ int game(char* fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], i
         count[player_index][value_index]++;
         cards++;
 
-        if(cards % NUM_PLAYERS == 0){ 
+        if (cards % NUM_PLAYERS == 0)
+        {
             winner_by_4_cards = check_winner_by_4cards(count, players);
-            if (winner_by_4_cards != -1) {
+            if (winner_by_4_cards != -1)
+            {
                 fclose(file);
                 return winner_by_4_cards;
             }
@@ -83,8 +111,10 @@ int game(char* fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], i
 
     int result[NUM_PLAYERS] = {0};
     int color_scores[NUM_PLAYERS] = {0};
-    for (int i = 0; i < NUM_PLAYERS; i++) {
-        for (int j = 0; j < player_card_count[i]; j++) {
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        for (int j = 0; j < player_card_count[i]; j++)
+        {
             result[i] += card_value(players[i][j].value);
             color_scores[i] += color_value(players[i][j].color);
         }
@@ -93,20 +123,27 @@ int game(char* fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], i
     int max_score = result[0];
     int winner = 0;
     int tie = 0;
-    for (int i = 1; i < NUM_PLAYERS; i++) {
-        if (result[i] > max_score) {
+    for (int i = 1; i < NUM_PLAYERS; i++)
+    {
+        if (result[i] > max_score)
+        {
             max_score = result[i];
             winner = i;
             tie = 0;
-        } else if (result[i] == max_score) {
+        }
+        else if (result[i] == max_score)
+        {
             tie = 1;
         }
     }
 
-    if (tie) {
+    if (tie)
+    {
         int max_color_score = color_scores[winner];
-        for (int i = 0; i < NUM_PLAYERS; i++) {
-            if (result[i] == max_score && color_scores[i] > max_color_score) {
+        for (int i = 0; i < NUM_PLAYERS; i++)
+        {
+            if (result[i] == max_score && color_scores[i] > max_color_score)
+            {
                 winner = i;
                 max_color_score = color_scores[i];
             }
@@ -116,12 +153,16 @@ int game(char* fileName, struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE], i
     return winner;
 }
 
-void sort_cards(struct Card cards[PLAYERS_ARRAY_SIZE], int turns) {
-    for (int i = 0; i < turns; i++) {
-        for (int j = i + 1; j < turns; j++) {
+void sort_cards(struct Card cards[PLAYERS_ARRAY_SIZE], int turns)
+{
+    for (int i = 0; i < turns; i++)
+    {
+        for (int j = i + 1; j < turns; j++)
+        {
             if (card_value(cards[i].value) < card_value(cards[j].value) ||
                 (card_value(cards[i].value) == card_value(cards[j].value) &&
-                 color_value(cards[i].color) < color_value(cards[j].color))) {
+                 color_value(cards[i].color) < color_value(cards[j].color)))
+            {
                 struct Card tmp = cards[i];
                 cards[i] = cards[j];
                 cards[j] = tmp;
@@ -130,21 +171,151 @@ void sort_cards(struct Card cards[PLAYERS_ARRAY_SIZE], int turns) {
     }
 }
 
-void print_winners_array(int winner_index, struct Card cards[PLAYERS_ARRAY_SIZE], int turns) {
+void print_winners_array(int winner_index, struct Card cards[PLAYERS_ARRAY_SIZE], int turns)
+{
     printf("Player %d wins: ", winner_index + 1);
     sort_cards(cards, turns);
-    for (int i = 0; i < turns; i++) {
-        printf("(%c %c); ", cards[i].value, cards[i].color);
+    for (int i = 0; i < turns; i++)
+    {
+        printf("(%c, %c), ", cards[i].value, cards[i].color);
     }
     printf("\n");
 }
 
-int main() {
+void append(char *s, char c)
+{
+    int len = strlen(s);
+    s[len] = c;
+    s[len + 1] = '\0';
+}
+
+char *get_file_content(char *file_name)
+{
+    int BUZZ_SIZE = 1024;
+    char buff[BUZZ_SIZE];
+    FILE *f = fopen(file_name, "r");
+    if (f == NULL)
+    {
+        printf("Error opening file %s", file_name);
+        exit(-1);
+    }
+
+    char *result = fgets(buff, BUZZ_SIZE, f);
+    // printf("String read: %s\n", buff);
+    fclose(f);
+
+    return result;
+}
+
+char *generate_key(char *plaintext, char *key)
+{
+    int plaintext_len = strlen(plaintext);
+    char *gen_key = malloc((plaintext_len) * sizeof(char));
+    int key_len = strlen(key);
+
+    for (int i = 0, j = 0; i < plaintext_len; i++, j++)
+    {
+        if (key_len == i || key[j] == '\0')
+        {
+            j = 0;
+        }
+
+        gen_key[i] = key[j];
+    }
+    gen_key[plaintext_len] = '\0';
+    return gen_key;
+}
+
+char *encrypt_text(char *plain_text, char *key)
+{
+    int len = strlen(plain_text);
+    char *cipher_text = (char *)malloc((len) * sizeof(char));
+    if (cipher_text == NULL)
+    {
+        printf("Error allocating memory for array");
+    }
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(plain_text[i]) != 0)
+        {
+            //vigenere cipher
+            char x = (plain_text[i] + key[i]) % 26;
+            x += 'A';
+            cipher_text[i] = x;
+        }
+        else
+        {
+            cipher_text[i] = plain_text[i];
+        }
+    }
+    cipher_text[len] = '\0';
+    return cipher_text;
+}
+
+char *decrypt_text(char *cipher_text, char *key)
+{
+    int len = strlen(cipher_text);
+    char *plain_text = (char *)malloc((len) * sizeof(char));
+
+    for (int i = 0; i < strlen(cipher_text); i++)
+    {
+        if (isalpha(cipher_text[i]) != 0)
+        {
+            char x = (cipher_text[i] - key[i] + 26) % 26;
+            x += 'A';
+            plain_text[i] = x;
+        }
+        else
+        {
+            plain_text[i] = cipher_text[i];
+        }
+    }
+    plain_text[len] = '\0';
+    return plain_text;
+}
+
+void write_to_file(char *file_name, char *data)
+{
+    FILE *f = fopen(file_name, "w");
+    if (f == NULL)
+    {
+        printf("Error opening file %s", file_name);
+        exit(-1);
+    }
+    fprintf(f, data);
+
+    fclose(f);
+}
+
+int main()
+{
+    char* key;
+    printf("Enter key (uppercase only!): ");
+    scanf("%s", key);
+
+    char *content = get_file_content("teste.txt");
+
+    char *gen_key = generate_key(content, key);
+    // printf("GEN KEY %s\n", gen_key);
+
+    char *encrypted = encrypt_text(content, gen_key);
+    // printf("ENCRIPTED %s\n", encrypted);
+
+    write_to_file("teste_encrypted.txt", encrypted);
+
+    char *decrypted = decrypt_text(encrypted, gen_key);
+    // printf("DECRIPTED %s\n", decrypted);
+
+    write_to_file("game_data.txt", decrypted);
+
     struct Card players[NUM_PLAYERS][PLAYERS_ARRAY_SIZE] = {0};
     int player_card_count[NUM_PLAYERS] = {0};
-    int winner_index = game("teste.txt", players, player_card_count);
-    
+    int winner_index = game("game_data.txt", players, player_card_count);
+
     print_winners_array(winner_index, players[winner_index], player_card_count[winner_index]);
-    
+
+    free(gen_key);
+    free(encrypted);
+    free(decrypted);
     return 0;
 }
